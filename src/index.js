1fa79427ecb70966d5413fab049d9255ad65c9aa
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-03-28 14:26:52 +0430

    Add fetch news api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {});
+import { apiURL } from "./newsUrl.js";
+const main = document.getElementById("main-articles");
+
+document.addEventListener("DOMContentLoaded", () => {
+  updateActicles();
+});
+
+async function updateActicles() {
+  const res = await fetch(apiURL);
+  const results = await res.json();
+
+  main.innerHTML = results.articles.map(createArticle).join("\n");
+}
+
+function createArticle(article) {
+  return `
+      <div class="article">
+        <a href="${article.url}">
+          <h2>${article.title}</h2>
+          <img src="${article.urlToImage}" alt="" />
+          <p>${article.description}</p>
+        </a>
+    </div>
+  `;
+}
